08126f2a628769c2493aca016eb07c2b64b420a7
Th3freeman <thefrman345@gmail.com>
2019-10-28 01:15:30 -0400

    added unique id's to each form and added script to save the form contents of each form into local storage when the save button is hit

--- java/java.js
+++ java/java.js
@@ -1,3 +1,32 @@
 var currentDateAndTime = Date(Date.now());
 
-$(".current_date").text(currentDateAndTime)
+$(".current_date").text(currentDateAndTime);
+
+function savePlans() {
+    localStorage.am9=$("#9am").val();
+    localStorage.am10=$("#10am").val();
+    localStorage.am11=$("#11am").val();
+    localStorage.pm12=$("#12pm").val();
+    localStorage.pm1=$("#1pm").val();
+    localStorage.pm2=$("#2pm").val();
+    localStorage.pm3=$("#3pm").val();
+    localStorage.pm4=$("#4pm").val();
+    localStorage.pm5=$("#5pm").val();
+    
+};
+
+function loadPlans() {
+    $("#9am").val(localStorage.am9);
+    $("#10am").val(localStorage.am10);
+    $("#11am").val(localStorage.am11);
+    $("#12pm").val(localStorage.pm12);
+    $("#1pm").val(localStorage.pm1);
+    $("#2pm").val(localStorage.pm2);
+    $("#3pm").val(localStorage.pm3);
+    $("#4pm").val(localStorage.pm4);
+    $("#5pm").val(localStorage.pm5);
+};
+
+
+loadPlans();
+
